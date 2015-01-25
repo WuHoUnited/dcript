cbb143abe8e3158aa7c368d397987e51573bf760
WuHoUnited <iforgotmypassphrase@gmail.com>
2015-01-25 17:09:32 -0500

    Updated readme and project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dcript "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Clojurecsript library for helping to solve substitution ciphers"
+  :url "https://github.com/wuhounited/dcript"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2511"]
